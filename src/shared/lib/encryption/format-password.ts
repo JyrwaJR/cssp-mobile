69d15426662643aa26c878d78f2c463ef7909c49
JyrwaJR <jyrwaboys@gmail.com>
2026-08-25 20:32:14 +0530

    update

--- src/shared/lib/encryption/format-password.ts
+++ src/shared/lib/encryption/format-password.ts
@@ -1,35 +1,59 @@
-import CryptoJS from 'crypto-js';
+import * as Crypto from 'expo-crypto';
+import { encode as base64Encode } from 'base-64';
 
 function randomBetween(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export function formatPassword(password: string): string {
-  const random = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+export async function formatPassword(password: string): Promise<string> {
+  const random: string[] = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+  ];
 
-  const randomSep = ['z', 'y', 'x', 'u', 'v', 'm'];
+  const randomSep: string[] = ['z', 'y', 'x', 'u', 'v', 'm'];
 
   const salt: (number | string)[] = [];
 
-  // Same as:
-  // createHash('sha256')
-  //   .update(password, 'utf8')
-  //   .digest('hex')
-  const hash = CryptoJS.SHA256(CryptoJS.enc.Utf8.parse(password)).toString(CryptoJS.enc.Hex);
+  const hash = await Crypto.digestStringAsync(Crypto.CryptoDigestAlgorithm.SHA256, password, {
+    encoding: Crypto.CryptoEncoding.HEX,
+  });
 
-  const pwd = hash.split('');
+  const pwd: string[] = hash.split('');
 
   for (let i = 0; i < 20; i++) {
-    const j = i <= 3 ? i : randomBetween(4, 63);
+    let j: number;
+
+    if (i <= 3) {
+      j = i;
+    } else {
+      j = randomBetween(4, 63);
+    }
 
     const temp = pwd[j];
 
     pwd[j] = random[Math.floor(Math.random() * random.length)];
 
-    salt.push(j, temp, randomSep[Math.floor(Math.random() * randomSep.length)]);
+    salt.push(j);
+    salt.push(temp);
+    salt.push(randomSep[Math.floor(Math.random() * randomSep.length)]);
   }
 
   const result = pwd.join('') + salt.join('');
 
-  return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(result));
+  return base64Encode(result);
 }
