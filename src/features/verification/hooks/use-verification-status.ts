4f86b0613944679b01c996a11e7bf948ad5d5c04
nongrum1998 <1998nongrum@gmail.com>
2026-08-26 16:45:54 +0530

    chane the data output and re trim it

--- src/features/verification/hooks/use-verification-status.ts
+++ src/features/verification/hooks/use-verification-status.ts
@@ -1,17 +1,34 @@
+import { useAuthStore } from '@stores/auth.store';
 import { useQuery } from '@tanstack/react-query';
 import { http } from '@utils/http';
 import { VerificationStatusT } from '../types';
 import { ENDPOINTS } from '@utils/constants';
-import { useAuthStore } from '@stores/auth.store';
 
 export function useVerificationStatus() {
   const { user, isSignedIn } = useAuthStore();
+
   const ppo_no = user?.ppo_no || process.env.EXPO_PUBLIC_PPO_NO;
+
   const isEnabled = process.env.NODE_ENV === 'development' ? true : isSignedIn && !!ppo_no;
+
   return useQuery({
     queryKey: ['verificationStatus', ppo_no],
+
     queryFn: () => http.post<VerificationStatusT>(ENDPOINTS.VERIFICATION.STATUS, { ppo_no }),
-    select: (data) => data.data,
+
+    select: (response) => {
+      const data = response.data;
+      if (data) {
+        return {
+          ...data,
+          ver_status: data?.ver_status
+            ?.replace(/\r?\n|\r/g, ' ')
+            .replace(/\s+/g, ' ')
+            .trim(),
+        };
+      }
+    },
+
     enabled: isEnabled,
   });
 }
