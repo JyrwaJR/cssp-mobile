fdf321a74a39093980aa2a39884712e522b70b94
nongrum1998 <1998nongrum@gmail.com>
2026-08-27 15:36:26 +0530

    extends user data shape

--- src/shared/types/auth/index.ts
+++ src/shared/types/auth/index.ts
@@ -2,8 +2,10 @@ import { LoginT } from './login';
 
 export type RoleT = 'USER' | 'MEMBER';
 
-export interface UserT extends LoginT { 
-  ppo_no:string
+export interface UserT extends LoginT {
+  ppo_no: string;
+  phone_no?: string;
+  organization?: string;
 }
 
 export * from './login';
