aeeaf3817b464a6cd67c951106f14fb960b43c91
nongrum1998 <1998nongrum@gmail.com>
2026-08-27 16:43:36 +0530

    chore(auth-provider): remove verbose hydration log statements

--- src/shared/components/providers/auth-provider.tsx
+++ src/shared/components/providers/auth-provider.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useAuthStore } from '@stores/auth.store';
-import { logger } from '@utils/logger';
 
 type Props = {
   children: React.ReactNode;
@@ -10,14 +9,10 @@ export const AuthInitializer = ({ children }: Props) => {
   const hydrate = useAuthStore((s) => s._hydrate);
 
   useEffect(() => {
-    logger.info('AuthInitializer: checking persist hydration');
     if (useAuthStore.persist.hasHydrated()) {
-      logger.info('AuthInitializer: already hydrated, calling _hydrate');
       hydrate();
     } else {
-      logger.info('AuthInitializer: waiting for hydration');
       const unsub = useAuthStore.persist.onFinishHydration(() => {
-        logger.info('AuthInitializer: hydration finished, calling _hydrate');
         hydrate();
       });
       return () => unsub();
