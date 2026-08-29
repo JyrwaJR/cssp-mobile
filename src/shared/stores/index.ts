12cc70774e7b6e418b68dc30de4b68538f484456
JyrwaJR <jyrwaboys@gmail.com>
2026-08-29 08:04:25 +0530

    remove un suse code

--- src/shared/stores/index.ts
+++ src/shared/stores/index.ts
@@ -1,7 +1,5 @@
 export * from './auth.store';
 export * from './token.store';
 export * from './update.store';
-export * from './notification.store';
-export * from './rate-limit.store';
 export * from './snackbar.store';
 export * from './theme.store';
