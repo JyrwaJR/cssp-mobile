efefaac51bd91b910673accf88573d705158dcf4
nongrum1998 <1998nongrum@gmail.com>
2026-08-31 13:53:18 +0530

    feat(ui): update drawer item styling with press effects and swipe enabled

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -31,38 +31,51 @@ function CustomDrawerContent(props: any) {
         <DrawerItem
           label="Home"
           onPress={() => navigate(PAGE_ROUTES.HOME)}
-          pressOpacity={1}
-          pressColor={'none'}
+          pressOpacity={0.1}
+          pressColor={'#FFF'}
           icon={({ size, color }) => <Icon name="information-circle" size={size} color={color} />}
         />
         <DrawerItem
           label="Change Password"
+          pressOpacity={0.1}
+          pressColor={'#FFF'}
           onPress={() => navigate(PAGE_ROUTES.CHANGE_PASSWORD)}
           icon={({ size, color }) => <Icon name="user-unlock" size={size} color={color} />}
         />
         <DrawerItem
           label="Contact Us"
+          pressOpacity={0.1}
+          pressColor={'#FFF'}
           onPress={() => navigate(PAGE_ROUTES.CONTACT_US)}
           icon={({ size, color }) => <Icon name="contact-01" size={size} color={color} />}
         />
         <DrawerItem
           label="Withdrawal"
+          pressOpacity={0.1}
+          pressColor={'#FFF'}
           onPress={() => navigate(PAGE_ROUTES.WITHDRAWAL)}
           icon={({ size, color }) => <Icon name="property-delete" size={size} color={color} />}
         />
 
         <DrawerItem
           label="User Manual"
+          pressOpacity={0.1}
+          pressColor={'#FFF'}
           onPress={() => navigate(PAGE_ROUTES.USER_MANUAL)}
           icon={({ size, color }) => <Icon name="book-01" size={size} color={color} />}
         />
         <DrawerItem
           label="Privacy Policy"
           onPress={() => navigate(PAGE_ROUTES.PRIVACY)}
+          pressOpacity={0.1}
+          pressColor={'#FFF'}
+          style={{ borderRadius: 6 }}
           icon={({ size, color }) => <Icon name="shield" size={size} color={color} />}
         />
         <DrawerItem
           label="About"
+          pressOpacity={0.1}
+          pressColor={'#FFF'}
           onPress={() => navigate(PAGE_ROUTES.ABOUT_US)}
           icon={({ size, color }) => <Icon name="info" size={size} color={color} />}
         />
@@ -83,12 +96,13 @@ export default function DrawerLayout() {
     <Drawer
       drawerContent={(props) => <CustomDrawerContent {...props} />}
       screenOptions={{
+        swipeEnabled: true,
         headerShown: false,
         drawerActiveBackgroundColor: '#F0FDFA',
         drawerActiveTintColor: '#4297A0',
         drawerInactiveTintColor: '#475569',
         drawerItemStyle: {
-          borderRadius: 6,
+          borderRadius: 0,
           marginHorizontal: 12,
           marginVertical: 2,
         },
