29847b846ca6624429b93a2121479954bcd887c7
nongrum1998 <1998nongrum@gmail.com>
2026-09-08 15:19:50 +0530

    change max image size to 800 kb

--- src/shared/utils/constants/common.ts
+++ src/shared/utils/constants/common.ts
@@ -14,7 +14,7 @@ export const APP_VERSION = Application.nativeApplicationVersion || '3.0.0';
 /**
  * File size limits (in bytes).
  */
-export const MAX_IMAGE_SIZE_IN_KB = 1024; // 1 MB
+export const MAX_IMAGE_SIZE_IN_KB = 800; // 1 MB
 
 export const MAX_IMAGE_SIZE = 1 * 1024 * 1024; // 1 MB
 
