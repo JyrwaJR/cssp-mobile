32de40a8bb6c59257599e9691ca389554267f761
nongrum1998 <1998nongrum@gmail.com>
2026-09-15 13:18:04 +0530

    refactor: drop dat login onSuccess handler in favor of interceptor

--- src/features/auth/hooks/use-dat-login.ts
+++ src/features/auth/hooks/use-dat-login.ts
@@ -3,23 +3,12 @@ import { http } from '@utils/http';
 import { LoginInput } from '../validators';
 import { encryptText } from '@lib/encryption';
 import { ENDPOINTS } from '@utils/constants';
-import { TokenStoreManager } from '@stores/token.store';
-import { logger } from '@utils/logger';
 
 export function useDatLogin() {
   return useMutation({
     mutationFn: (data: LoginInput) => {
       const ecryptedData = encryptText(JSON.stringify(data));
       return http.post<{ token: string }>(ENDPOINTS.AUTH.DAT_LOGIN, { payload: ecryptedData });
-    },
-    onSuccess: async (data) => {
-      if (__DEV__) {
-        logger.info('Login Res with Second Login', data.success, data);
-      }
-      if (data.data?.token) {
-        await TokenStoreManager.addDatAccessToken(data?.data?.token);
-      }
-      return data;
     },
   });
 }
